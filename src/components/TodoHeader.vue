0229afaa81b5881926404ab8352189129e0c8525
Kola771 <koladeaboudou@gmail.com>
2024-07-12 16:46:51 +0200

    todoheader créé

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <RouterLink to="/">
+      <h1>Todos</h1>
+    </RouterLink>
+
+    <input
+      type="text"
+      class="new-todo"
+      autofocus
+      autocomplete="off"
+      placeholder="Entrez une tâche"
+    />
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
