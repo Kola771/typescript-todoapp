aeb12dcdabb4c21e5261f1e607fa1976d088d442
Kola771 <koladeaboudou@gmail.com>
2024-07-19 15:51:05 +0200

    deleteCompleted

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -6,27 +6,34 @@
     </span>
     <ul class="filters">
       <li>
-        <router-link to="/">Tous</router-link>
+        <router-link :class="{ selected: route.path === '/' }" to="/">Tous</router-link>
       </li>
       <li>
-        <router-link to="/waiting">En cours</router-link>
+        <router-link :class="{ selected: route.path === '/waiting' }" to="/waiting">En cours</router-link>
       </li>
       <li>
-        <router-link to="/completed">Terminés</router-link>
+        <router-link :class="{ selected: route.path === '/completed' }" to="/completed">Terminés</router-link>
       </li>
     </ul>
 
-    <button class="clear-completed">Eff. taches restantes</button>
+    <button class="clear-completed" @click="emit('delete-completed')">Eff. taches restantes</button>
   </footer>
 </template>
 
 <script setup lang="ts">
 import type { Todo } from '@/@types'
 import { computed, ref } from 'vue'
+import { useRoute } from 'vue-router'
 
 const props = defineProps<{
   todos: Todo[]
-}>()
+}>();
+
+const emit = defineEmits<{
+  (e: "delete-completed"): void
+}>();
+
+const route = useRoute()
 
 const remaining = computed(() => props.todos.filter((el) => !el.complete).length)
 </script>
--- src/components/TodosComponent.vue
+++ src/components/TodosComponent.vue
@@ -4,7 +4,7 @@
 
     <TodoMain :taches="filteredTodos" @delete-todo="deleteTodo" @update-todo="updateTodo" @edit-todo="editTodo" />
 
-    <TodoFooter :todos="todos" />
+    <TodoFooter :todos="todos" @delete-completed="deleteCompleted" />
   </div>
 </template>
 
@@ -14,36 +14,36 @@ import TodoMain from '@/components/TodoMain.vue'
 import TodoFooter from '@/components/TodoFooter.vue'
 import type { Todo } from '@/@types'
 import { computed, ref } from 'vue'
-import { useStorage } from '@vueuse/core';
-import { nanoid } from 'nanoid';
+import { useStorage } from '@vueuse/core'
+import { nanoid } from 'nanoid'
 import { useRoute } from 'vue-router'
 
-const route = useRoute();
+const route = useRoute()
 
 // const todos = ref<Todo[]>([]);
-const todos = useStorage<Todo[]>('todoapp-todos', []);
+const todos = useStorage<Todo[]>('todoapp-todos', [])
 
 const filters = computed(() => {
   return {
     all: todos,
     waiting: todos.value.filter((todo) => !todo.complete),
-    completed: todos.value.filter((todo) => todo.complete),
+    completed: todos.value.filter((todo) => todo.complete)
   }
-});
+})
 
-const waitingTodos = computed<Todo[]>(() => filters.value.waiting);
-const completedTodos = computed<Todo[]>(() => filters.value.completed);
+const waitingTodos = computed<Todo[]>(() => filters.value.waiting)
+const completedTodos = computed<Todo[]>(() => filters.value.completed)
 
 const filteredTodos = computed(() => {
   switch (route.name) {
     case 'waiting':
-      return waitingTodos.value;
+      return waitingTodos.value
 
     case 'completed':
-      return completedTodos.value;
+      return completedTodos.value
 
     default:
-      return todos.value;
+      return todos.value
   }
 })
 
@@ -63,12 +63,19 @@ function deleteTodo(value: Todo): void {
   todos.value = todos.value.filter((todo) => todo !== value)
 }
 
+// Fonction pour dire qu'une tâche est finie
 function updateTodo(todo: Todo, completedValue: boolean) {
   todo.complete = completedValue
 }
 
+// Fonction pour modifier le titre d'une tâche
 function editTodo(todo: Todo, value: string) {
   todo.title = value
+}
+
+// Fonction pour retirer toutes les tâches finies
+function deleteCompleted() {
+  todos.value = todos.value.filter((todo) => !todo.complete)
 }
 </script>
 
